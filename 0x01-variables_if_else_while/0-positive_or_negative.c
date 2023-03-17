9b74291da0ef8694435edbb862d1dffe7134ecbd
Sadickdb <sadickdixonb@gmail.com>
2023-03-17 15:23:02 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
  * main - shows a random number and states wether
- * 	it is positive, negative or zero.
+ *	it is positive, negative or zero.
  *
  * Return: Always 0.
  */
@@ -16,15 +16,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n > 0) {
-		printf("%d is positive\n", n);
+	if (n > 0)
+       	{
+		printf ("%d is positive\n", n);
 
 	}
-	else if(n < 0) {
+	else if(n < 0) 
+	{
 		printf("%d is negative\n", n);
 
 	}
-	else {
+	else
+       	{
 		printf("%d is zero\n", n);
 	}
 	return (0);
