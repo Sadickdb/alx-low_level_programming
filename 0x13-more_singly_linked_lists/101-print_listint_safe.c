52ede9f55fd0210ed0b6cd6bafd6a1f46e73103a
Sadickdb <sadickdixonb@gmail.com>
2023-05-02 08:05:26 -0700

    pushing task 102

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,28 +12,65 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current = head, *temp;
-	size_t count = 0;
+	listp_t *temp;
+	listp_t *curr;
 
-	while (current != NULL)
+	if (head != NULL)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
-		count++;
-
-		if (current <= current->next)
+		curr = *head;
+		while ((temp = curr) != NULL)
 		{
-			temp = current->next;
-			current = temp;
-			continue;
+			curr = curr->next;
+			free(temp);
 		}
-		break;
+		*head = NULL;
 	}
+}
 
-	if (current != NULL)
+/**
+ * free_listint_safe - frees a linked list.
+ * @h: head of a list.
+ *
+ * Return: size of the list that was freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t nnodes = 0;
+	listp_t *hptr, *new, *add;
+	listint_t *curr;
+
+	hptr = NULL;
+	while (*h != NULL)
 	{
-		printf("-> [%p] %d\n", (void *)current, current->n);
-		exit(98);
+		new = malloc(sizeof(listp_t));
+
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)*h;
+		new->next = hptr;
+		hptr = new;
+
+		add = hptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (*h == add->p)
+			{
+				*h = NULL;
+				free_listp2(&hptr);
+				return (nnodes);
+			}
+		}
+
+		curr = *h;
+		*h = (*h)->next;
+		free(curr);
+		nnodes++;
 	}
 
-	return (count);
+	*h = NULL;
+	free_listp2(&hptr);
+	return (nnodes);
 }
