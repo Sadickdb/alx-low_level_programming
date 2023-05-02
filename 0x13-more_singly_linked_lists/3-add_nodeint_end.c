efbcd8259ac0a438a4f212d79d99038d7d1c544f
Sadickdb <sadickdixonb@gmail.com>
2023-05-02 07:48:42 -0700

    pushing 0x13-mo

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,26 +10,31 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listin_t *new_node, *current;
+	listint_t *new;
+	listint_t *temp;
 
-	new_node = malloc(sizeof(listin_t));
-	if (new_node == NULL)
+	(void)temp;
+
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
 		return (NULL);
 
-	new_node->n = n;
-	mew_node->next = NULL;
-
+	new->n = n;
+	new->next = NULL;
+	temp = *head;
 	if (*head == NULL)
 	{
-		*head = new_node;
-		return (new_node);
+		*head = new;
+	}
+	else
+	{
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = new;
 	}
 
-	current = *head;
-	while (current->next != NULL)
-		current = current->next;
-
-	current->next = new_node;
-
-	return (new_node);
+	return (*head);
 }
